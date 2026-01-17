5e274fdf366ed27354c13a617b66949bd112eb13
DelioCoder <davideveriwhere@gmail.com>
2026-01-17 16:06:31 -0500

    Dto for employee init

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,1 +1,23 @@
-export class CreateEmployeeDto {}
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateEmployeeDto {
+
+    @IsString()
+    tipoDocumento: string;
+
+    @IsNumber()
+    numeroDocumento: number;
+
+    @IsString()
+    nombre: string;
+
+    @IsString()
+    apellido: string;
+
+    @IsString()
+    cargo: string;
+
+    @IsString()
+    fechaNacimiento: string;
+
+}
--- src/employees/dto/update-employee.dto.ts
+++ src/employees/dto/update-employee.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEmployeeDto } from './create-employee.dto';
+import { IsNumber, IsPositive } from 'class-validator';
 
-export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {}
+export class UpdateEmployeeDto extends PartialType(CreateEmployeeDto) {
+    
+    @IsNumber()
+    @IsPositive()
+    id: number;
+
+}
