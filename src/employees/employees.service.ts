f88c0b84f1f1c4e423d68c6023b39dcb12cebb9d
DelioCoder <davideveriwhere@gmail.com>
2026-01-17 16:06:24 -0500

    CRUD Employee

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,26 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { PrismaService } from 'src/prisma.service';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Injectable()
 export class EmployeesService {
-  create(createEmployeeDto: CreateEmployeeDto) {
-    return 'This action adds a new employee';
+  
+  private readonly logger = new Logger('EmployeeService');
+
+  constructor(private prisma: PrismaService) { }
+
+  async create(createEmployeeDto: CreateEmployeeDto) {
+    return await this.prisma.employee.create({
+      data: {
+        ...createEmployeeDto,
+        fechaNacimiento: new Date(createEmployeeDto.fechaNacimiento)
+      }
+    });
   }
 
-  findAll() {
-    return `This action returns all employees`;
+  async findAll(paginationDto: PaginationDto) {
+
+    const { limit, page } = paginationDto;
+
+    const totalPages = await this.prisma.employee.count();
+    const lastPage = Math.ceil(totalPages/limit!);
+
+    return await this.prisma.employee.findMany({
+      take: limit,
+      skip: ( page! - 1 ) * limit!
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} employee`;
+  async findOne(id: number) {
+    const employee =  await this.prisma.employee.findUnique({
+      where: {
+        id
+      }
+    });
+
+    if(!employee) throw new NotFoundException({
+      message: `Employee with id: [${id}] not found`,
+      status: HttpStatus.NOT_FOUND
+    });
+
+    return employee;
   }
 
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
-    return `This action updates a #${id} employee`;
+  async update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+    
+    const { id: __, ...dto } = updateEmployeeDto;
+
+    await this.findOne(id);
+
+    if(Object.values(updateEmployeeDto).length === 0) {
+      return {
+        message: 'nothing for update',
+      }
+    }
+
+    return await this.prisma.employee.update({
+      where: { id },
+      data: {
+        ...dto,
+        fechaNacimiento: new Date(dto.fechaNacimiento!)
+      }
+    });
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} employee`;
+  async remove(id: number) {
+
+    await this.findOne(id);
+
+    return await this.prisma.employee.delete({
+      where: {
+        id
+      }
+    })
   }
 }
