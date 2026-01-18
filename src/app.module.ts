6bc30ee41cec9518f1b372958771d11610cac305
DelioCoder <davideveriwhere@gmail.com>
2026-01-18 12:50:08 -0500

    Redis configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { CacheModule } from '@nestjs/cache-manager';
+import KeyvRedis, { Keyv } from '@keyv/redis';
+import { CacheableMemory } from 'cacheable';
 import { EmployeesModule } from './employees/employees.module';
 import { PrismaService } from './prisma.service';
-import { ThrottlerModule } from '@nestjs/throttler';
 import { SeedModule } from './seed/seed.module';
+import { envs } from './config/envs';
 
 @Module({
   imports: [
+    // Rate Limit
     ThrottlerModule.forRoot({
       throttlers: [
         {
-          ttl: 60000,
+          ttl: 60,
           limit: 10,
         }
       ]
+    }),
+    // Redis
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => {
+        return {
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
+            }),
+            new KeyvRedis(envs.redisUrl)
+          ]
+        }
+      }
     }),
     EmployeesModule,
     SeedModule,
