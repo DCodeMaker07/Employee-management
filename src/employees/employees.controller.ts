ec3826065347a7b1568200ee857791d1ebfcc08b
DelioCoder <davideveriwhere@gmail.com>
2026-01-24 12:54:13 -0500

    Auth module imported

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -3,12 +3,15 @@ import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { PaginationDto } from 'src/common';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ValidRoles } from 'src/auth/interfaces';
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
   }
@@ -24,11 +27,13 @@ export class EmployeesController {
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
     return this.employeesService.update(+id, updateEmployeeDto);
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id') id: string) {
     return this.employeesService.remove(+id);
   }
--- src/employees/employees.module.ts
+++ src/employees/employees.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { EmployeesController } from './employees.controller';
 import { PrismaService } from 'src/prisma.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [EmployeesController],
   providers: [EmployeesService, PrismaService],
+  imports: [AuthModule],
   exports: [EmployeesService]
 })
 export class EmployeesModule {}
